3b8bb48ba1fa576d86f102a3b3776b4ecd266b05
Franziska Graf <fgraf611@gmail.com>
2014-05-05 11:02:42 +0200

    no message

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,34 @@
 
 public class Board {
+	char [][] board;
+	String onMove;
+	int  MoveNum;
+	
+	public Board(){
+		board = new char[5] [6];
+		onMove = "W";
+		MoveNum = 0;
+	}
+	
+	public Board (String string){
+		
+		String [] [] square = {{"k", "q", "b", "n", "r"},
+				  {"p", "p", "p", "p", "p"},
+				  {".", ".", ".", ".", "."},
+				  {".", ".", ".", ".", "."},
+				  {"P", "P", "P", "P", "P"},
+				  {"R", "N","B", "Q", "K"}
+				  		
+		};
+	}
+	
+	public void makeBoard(String state){
+		
+	}
+	
+
+	
+	
+	
 
 }
